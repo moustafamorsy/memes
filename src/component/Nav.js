e6b96697e75b996b9f09ed81cf839dc5f72cbe24
moustafamorsy <mostafamorsyyy@gmail.com>
2022-08-05 03:22:15 +0200

    g

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,4 +1,4 @@
-import img from "./Troll Face.png"
+
 
 function Nav() {
 
@@ -20,7 +20,7 @@ const styles ={
 <nav style={styles}>
 
 <div style={{display: 'flex' ,alignItems: 'center'}} className='logo'>
-    <img style={{height:50,marginRight:10}} src={img} alt="smile head" />
+    <img style={{height:50,marginRight:10}} src="../face.png" alt="smile head" />
     <h1>Meme Generator</h1>
 </div>
 
